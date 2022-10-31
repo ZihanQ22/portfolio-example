dd2a4e3220fed55ddd48832e9528b89a973a5c6e
Doug Rosman <drosma@artic.edu>
2022-10-31 18:00:37 -0500

    finish starter scripts

--- scripts/header-footer.js
+++ scripts/header-footer.js
@@ -1,1 +1,19 @@
-const footer = `<footer>&copy; ${new Date().getFullYear()} Eduardo Kac</footer>`
+// HEADER
+$('#header-container').load('/components/header.html', function(){
+
+  // add code here for things that should happen once the <header> loads.
+
+  // for example, this is where you would add functionality for dealing with a toggle-able hamburger menu.
+
+
+
+});
+
+
+
+
+// FOOTER
+
+const footer = `<footer>&copy; ${new Date().getFullYear()} Portfolio Example</footer>`
+
+$('body').append(footer)
