4710e9e2636cde14034703e83ece52db6c6eaf6c
Doug Rosman <drosma@artic.edu>
2022-10-31 19:47:03 -0500

    update import order

--- scripts/import.js
+++ scripts/import.js
@@ -1,19 +1,10 @@
 
-// import scripts
 
-
-const SCRIPTS = [
-  `/scripts/header-footer.js`
-]
-
-SCRIPTS.forEach((script) => {
-  $.getScript(script);
-})
 
 // import stylesheets
 const STYLESHEETS = [
-  `<link rel="stylesheet" href="/stylesheets/site.css">`,
   `<link rel="stylesheet" href="/stylesheets/fonts.css">`,
+  `<link rel="stylesheet" href="/stylesheets/site.css">`,
   `<link rel="stylesheet" href="/stylesheets/header.css">`,
   `<link rel="stylesheet" href="/stylesheets/footer.css">`,
   `<link rel="stylesheet" href="style.css">`
@@ -23,4 +14,16 @@ STYLESHEETS.forEach((stylesheet) => {
 
   $('head').append(stylesheet);
   
+})
+
+const SCRIPTS = [
+  `/scripts/header-footer.js`
+]
+
+// import scripts
+
+
+
+SCRIPTS.forEach((script) => {
+  $.getScript(script);
 })
