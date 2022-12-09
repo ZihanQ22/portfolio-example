5e80b3db53ef0ac26ff70efec123be3ac240ef8b
dougrosman-edu <drosma@artic.edu>
2022-12-08 22:31:08 -0600

    upload stuff without the large image

---
+++ works/large-image-generator/sketch.js
@@ -0,0 +1,18 @@
+// function setup() {
+//   createCanvas(5120, 5120);
+//   pixelDensity(1);
+
+//   loadPixels();
+
+//   for(let i = 0; i < pixels.length; i++) {
+//     pixels[i] = floor(random(256));
+//     pixels[i+1] = floor(random(256));
+//     pixels[i+2] = floor(random(256));
+//     pixels[i+3] = 255;
+//   }
+
+//   updatePixels();
+
+//   save("cool.png");
+
+// }
