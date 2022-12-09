c5939589f712082dd673fc837973f2cc6608f72a
ZihanQ22 <zqiu3@artic.edu>
2022-12-09 16:08:17 -0600

    add stuff

--- works/toggle-galleries/main.js
+++ works/toggle-galleries/main.js
@@ -24,3 +24,17 @@ $('.gallery-link').click(function () {
     $(".gallery-container").not(`${selectedGallery}`).hide();
 })
 
+const menu = `
+<nav class="main-nav">
+  <a href="/">Portfolio</a>
+  <a href="/bio">Bio</a>
+  <a href="/contact">Contact</a>
+  <a href="/sure">Sure</a>
+</nav>
+`
+
+let menuContainer = document.getElementById("menu-container");
+menuContainer.innerHTML = menu;
+
+let footerMenuContainer = document.getElementById("footer-menu-container");
+footerMenuContainer = menu;
